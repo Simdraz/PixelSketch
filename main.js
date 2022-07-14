2df57b157a3b782e6f4d39856b2cfdeab55cf51c
Simdraz <simon.draznik139@gmail.com>
2022-07-14 23:10:57 +0200

    Created colorPixel() call function that colors divs on hover

--- main.js
+++ main.js
@@ -4,6 +4,19 @@ for (let i=0; i<16*16; i++) {
   createDiv();
 }
 
+const pixels = Array.from(document.querySelectorAll('.pixel'));
+
+pixels.forEach( pixel => pixel.addEventListener('mouseover', colorPixel));
+
+
+
+
+
+function colorPixel (e) {
+  this.style.background = "black";
+
+}
+
 function createDiv () {
   const div = document.createElement('div');
   div.classList.add("pixel");
